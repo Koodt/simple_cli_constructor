26d470e04770632d38f88120be86f1e89349d944
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-25 00:25:09 +0300

    master: function sending try

--- CliConstructor.py
+++ CliConstructor.py
@@ -11,19 +11,23 @@ class CliConstructor(object):
         while self.cli != 'exit':
             self.cli = input(self.prompt).lower()
             try:
-                getattr(self, self.cli)()
+#                getattr(self, self.cli)()
+                self.top_level[self.cli]
             except:
                 pass
 
         sys.exit('Graceful exit')
 
-    def add(self, name, description, parrent = None):
+    def add(self, name, description, parrent, func, arg):
         self.name = name
         self.description = description
         self.parrent = parrent
+        self.func = func
+        self.arg = arg
 
         if not self.parrent:
             self.top_level[self.name] = self.description
+            self.top_level[self.cli] = self.func
 
     def help(self):
         for item in self.top_level:
--- cli_testing.py
+++ cli_testing.py
@@ -1,8 +1,11 @@
 from CliConstructor import CliConstructor
 
+def func(arg):
+    print(arg)
+
 
 local_cli = CliConstructor()
-local_cli.add('add', 'adding element')
-local_cli.add('del', 'delete element')
-local_cli.add('list', 'list elements')
+local_cli.add('add', 'adding element', None, func, 'Jjfmr')
+local_cli.add('del', 'delete element', None, func, 'dsfgdfth')
+local_cli.add('list', 'list elements', None, func, 'sgdg')
 local_cli.initial('my_cli>')
