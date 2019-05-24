ff44f51ae8289c1e96cb8eeea452bb2389341add
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-24 16:43:08 +0300

    master: add adding function

--- CliConstructor.py
+++ CliConstructor.py
@@ -17,6 +17,14 @@ class CliConstructor(object):
 
         sys.exit('Graceful exit')
 
+    def add(self, name, description, parrent = None):
+        self.name = name
+        self.description = description
+        self.parrent = parrent
+
+        if not self.parrent:
+            self.top_level[self.name] = self.description
+
     def help(self):
         for item in self.top_level:
             print(item, '\t\t', self.top_level[item])
