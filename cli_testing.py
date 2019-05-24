ab24933141e32a13e75898728e3ff1c24cbb2316
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-24 16:43:22 +0300

    testing: fix testing

--- cli_testing.py
+++ cli_testing.py
@@ -2,4 +2,7 @@ from CliConstructor import CliConstructor
 
 
 local_cli = CliConstructor()
+local_cli.add('add', 'adding element')
+local_cli.add('del', 'delete element')
+local_cli.add('list', 'list elements')
 local_cli.initial('my_cli>')
