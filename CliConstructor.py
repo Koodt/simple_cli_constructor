22916d54723b8df5fd00485f2415da16b54825fa
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-24 14:43:28 +0300

    master: add initial code

---
+++ CliConstructor.py
@@ -0,0 +1,28 @@
+class CliConstructor(object):
+    def __init__(self):
+        self.cli = ''.lower()
+
+    def initial(self, prompt):
+        import sys
+        self.prompt = prompt
+        while self.cli != 'exit':
+            self.cli = input(self.prompt).lower()
+            try:
+                getattr(self, self.cli)()
+            except:
+                if self.cli != 'exit':
+                    print('Command not found')
+        sys.exit('Graceful exit')
+
+    def add(self):
+        pass
+
+    def help(self):
+        print('''
+        kCLI helper:
+        help        -       print this message
+        create      -       create config
+        add         -       add service to config
+        del         -       del service from config
+        exit        -       exit from kCLI
+        ''')
