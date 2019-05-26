8753ae0dbc91abae636892afeb499beb1024d7fc
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-26 10:12:15 +0300

    master: send multiply args to function

--- CliConstructor.py
+++ CliConstructor.py
@@ -1,40 +1,49 @@
 class CliConstructor(object):
     def __init__(self):
         self.top_level = {'help': 'print this menu', 'exit': 'quit programm'}
-        self.func_array = {'help': self.help}
+        self.func_array = {'help': self.cli_help}
         self.arg_array = {'help': False}
 
-    def initial(self, prompt):
+    def run_cli(self, prompt):
         import sys
         self.cli = ' '.lower()
         self.prompt = prompt
 
         while self.cli != 'exit':
-            self.cli = input(self.prompt).lower()
+            self.cli = str(input(self.prompt).lower())
+            words = self.cli.split()
+
             try:
-#                getattr(self, self.cli)()
                 if self.arg_array[self.cli]:
                     self.func_array[self.cli](self.arg_array[self.cli])
                 else:
                     self.func_array[self.cli]()
 
             except:
-                pass
+                if self.cli == '' or self.cli == 'exit':
+                    pass
+                else:
+                    print('Command not found')
 
         sys.exit('Graceful exit')
 
-    def add(self, name, description, parrent, func, arg):
+    def add_cli_item(self, name, description, parrent = False, func = False, args = False):
         self.name = name
         self.description = description
         self.parrent = parrent
         self.func = func
-        self.arg = arg
+        self.args = args
+        if type(self.args) is set:
+            pass
 
         if not self.parrent:
             self.top_level[self.name] = self.description
             self.func_array[self.name] = self.func
-            self.arg_array[self.name] = self.arg
+            self.arg_array[self.name] = self.args
 
-    def help(self):
+    def cli_return(self):
+        pass
+
+    def cli_help(self):
         for item in self.top_level:
             print(item, '\t\t', self.top_level[item])
--- cli_testing.py
+++ cli_testing.py
@@ -3,11 +3,11 @@ from CliConstructor import CliConstructor
 def func(arg):
     print(arg)
 
-def func_to(arg):
+def func_too(arg):
     print(arg)
 
 local_cli = CliConstructor()
-local_cli.add('add', 'adding element', None, func, 'Jjfmr')
-local_cli.add('del', 'delete element', None, func_to, 'ChECK')
-local_cli.add('list', 'list elements', None, func, 'sgdg')
-local_cli.initial('my_cli>')
+local_cli.add_cli_item('add', 'adding element', None, func, 'Jjfmr')
+local_cli.add_cli_item('del', 'delete element', None, func_too, {'de', 'defe'})
+local_cli.add_cli_item('list', 'list elements', None, func, 'sgdg')
+local_cli.run_cli('my_cli>')
